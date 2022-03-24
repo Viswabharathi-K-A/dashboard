02d1093f544022a1a6a752236192416cafa3734e
Viswabharathi-K-A <viswabharathika@gmail.com>
2022-03-25 00:11:22 +0530

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,140 @@
+import 'package:dashboard/common/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'controllers/menu_controller.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:dashboard/dashboard.dart';
 
-import 'pages/home_page.dart';
+void main() => runApp(const login());
 
-void main() {
-  runApp(MyApp());
-}
+class login extends StatelessWidget {
+  const login();
 
-/// Let's start to make responsive website
-/// First make app responsive class
-
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dashboard',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => MenuController()),
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/welcome': (context) => WelcomeScreen(),
+      },
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.bgSideMenu,
+      body:  Column(
+          children: [
+          Center(
+           child:SizedBox(
+            height: 400,
+            width: 400,
+            child: Image.asset('assets/notification_image.png'),
+          ),),
+          SizedBox(
+          width: 400,
+          child: Card(
+            child: loginform(),
+          ),
+        ),
+        ]
+        ),
+        );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+class loginform extends StatefulWidget {
+  const loginform();
+
+  @override
+  _loginformState createState() => _loginformState();
+}
+
+class _loginformState extends State<loginform> {
+  final _username = TextEditingController();
+  final _passwaord = TextEditingController();
+  double _formProgress = 0;
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _username,
+      _passwaord,
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
+  void _showWelcomeScreen() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => dashboard()));// redirect to dashboard student or faculty //
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      onChanged: _updateFormProgress,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LinearProgressIndicator(value: _formProgress),
+          Text('Welcome', style: Theme.of(context).textTheme.headline4),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _username,
+              decoration: const InputDecoration(hintText: 'username'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: _passwaord,
+              obscureText: true,
+              decoration: const InputDecoration(hintText: 'password'),
+            ),
+          ),
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled)
+                    ? null
+                    : Colors.white;
+              }),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                return states.contains(MaterialState.disabled)
+                    ? null
+                    : Colors.blueAccent;
+              }),
+            ),
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
+            child: const Text('login'),
+          ),
         ],
-        child: HomePage(),
       ),
     );
   }
